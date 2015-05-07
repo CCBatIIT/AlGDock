f2980b810515cb82081727047b07aaa90f0c0a76
nguyentrunghai <nguyentrungh@gmail.com>
2015-05-07 15:38:47 -0500

    Implemented genetic MC

--- AlGDock/BindingPMF.py
+++ AlGDock/BindingPMF.py
@@ -131,6 +131,11 @@ arguments = {
     'help':'Keep configurations for intermediate states?'},
   'min_repX_acc':{'type':float,
     'help':'Minimum value for replica exchange acceptance rate'},
+  # for GMC
+  'GMC_attempts':{'type':int, 'default': 0,
+                  'help': 'Number of attempts is K * GMC_attempts. Zero means not to do GMC' },
+  'GMC_tors_threshold': {'type':float, 'default': 0.0,
+                  'help': 'The torsion threshold (in radian) below which no crossover will be attempted' },
   # For postprocessing
   'phases':{'nargs':'+', 'help':'Phases to use in postprocessing'},
   'rmsd':{'nargs':'?', 'const':True, 'default':False,
@@ -464,7 +469,9 @@ last modified {2}
         'attempts_per_sweep':100,
         'steps_per_sweep':1000,
         'phases':['NAMD_Gas','NAMD_GBSA'],
-        'keep_intermediate':False}
+        'keep_intermediate':False,
+        'GMC_attempts': 0,
+        'GMC_tors_threshold': 0.0 }
 
     args['default_dock'] = dict(args['default_cool'].items() + {
       'site':None, 'site_center':None, 'site_direction':None,
@@ -1971,7 +1978,122 @@ last modified {2}
     """
     if not process in ['dock','cool']:
       raise Exception('Process must be dock or cool')
-
+# GMC
+    def gMC_initial_setup():
+      """
+      Initialize BAT converter object.
+      Decide which internal coord to crossover. Here, only the soft torsions will be crossovered.
+      Produce a list of replica (state) index pairs to be swaped. Only Neighbor pairs will be swaped.
+      Assume that self.universe, self.molecule and K (number of states) exist
+      as global variables when the function is called.
+      """
+      from AlGDock.RigidBodies import identifier
+      import itertools
+      BAT_converter = identifier( self.universe, self.molecule )
+      BAT = BAT_converter.BAT( extended = True )
+      # this assumes that the torsional angles are stored in the tail of BAT
+      softTorsionId = [ i + len(BAT) - BAT_converter.ntorsions for i in BAT_converter._softTorsionInd ]
+      torsions_to_crossover = []
+      for i in range(1, len(softTorsionId) ):
+        combinations = itertools.combinations( softTorsionId, i )
+        for c in combinations:
+          torsions_to_crossover.append( list(c) )
+      #
+      BAT_converter.BAT_to_crossover = torsions_to_crossover
+      if len( BAT_converter.BAT_to_crossover ) == 0:
+        print '  GMC No BAT to crossover.'
+      state_indices = range( K )
+      state_indices_to_swap = zip( state_indices[0::2], state_indices[1::2] ) + \
+                      zip( state_indices[1::2], state_indices[2::2] )
+      #
+      return BAT_converter, state_indices_to_swap
+    #
+    def do_gMC( nr_attempts, BAT_converter, state_indices_to_swap, torsion_threshold ):
+      """
+      Assume self.universe, confs, lambdas, state_inds, inv_state_inds exist as global variables
+      when the function is called.
+      If at least one of the torsions in the combination chosen for an crossover attempt
+      changes more than torsion_threshold, the crossover will be attempted.
+      The function will update confs.
+      It returns the number of attempts and the number of accepted moves.
+      """
+      if nr_attempts < 0:
+        raise Exception('Number of attempts must be nonnegative!')
+      if torsion_threshold < 0.:
+        raise Exception('Torsion threshold must be nonnegative!')
+      #
+      if len( BAT_converter.BAT_to_crossover ) == 0:
+        return 0., 0.
+      #
+      from random import randrange
+      # get reduced energies and BAT for all configurations in confs
+      BATs = []
+      energies = np.zeros( K, dtype = float )
+      for c_ind in range(K):
+        s_ind = state_inds[ c_ind ]
+        self.universe.setConfiguration( Configuration( self.universe, confs[c_ind] ) )
+        BATs.append( np.array( BAT_converter.BAT( extended = True ) , dtype = float ) )
+        self._set_universe_evaluator( lambdas[ s_ind ] )
+        reduced_e = self.universe.energy() / ( R*lambdas[ s_ind ]['T'] )
+        energies[ c_ind ] = reduced_e
+      #
+      nr_sets_of_torsions = len( BAT_converter.BAT_to_crossover )
+      #
+      attempt_count , acc_count = 0 , 0
+      sweep_count = 0
+      while True:
+        sweep_count += 1
+        if (sweep_count * K) > (1000 * nr_attempts):
+          print '  GMC Sweep too many times, but few attempted. Consider reducing torsion_threshold.'
+          return attempt_count, acc_count
+        #
+        for state_pair in state_indices_to_swap:
+          conf_ind_k0 = inv_state_inds[ state_pair[0] ]
+          conf_ind_k1 = inv_state_inds[ state_pair[1] ]
+          # check if it should attempt for this pair of states
+          ran_set_torsions = BAT_converter.BAT_to_crossover[ randrange( nr_sets_of_torsions ) ]
+          do_crossover = np.any(np.abs(BATs[conf_ind_k0][ran_set_torsions] - BATs[conf_ind_k1][ran_set_torsions]) >= torsion_threshold)
+          if do_crossover:
+            attempt_count += 1
+            # BAT and reduced energies before crossover
+            BAT_k0_be = copy.deepcopy( BATs[conf_ind_k0] )
+            BAT_k1_be = copy.deepcopy( BATs[conf_ind_k1] )
+            e_k0_be = energies[conf_ind_k0]
+            e_k1_be = energies[conf_ind_k1]
+            # BAT after crossover
+            BAT_k0_af = copy.deepcopy( BAT_k0_be )
+            BAT_k1_af = copy.deepcopy( BAT_k1_be )
+            for index in ran_set_torsions:
+              tmp = BAT_k0_af[ index ]
+              BAT_k0_af[ index ] = BAT_k1_af[ index ]
+              BAT_k1_af[ index ] = tmp
+            # Cartesian coord and reduced energies after crossover.
+            BAT_converter.Cartesian( BAT_k0_af )
+            self._set_universe_evaluator( lambdas[ state_pair[0] ] )
+            e_k0_af = self.universe.energy() / ( R*lambdas[ state_pair[0] ]['T'] )
+            conf_k0_af = copy.deepcopy( self.universe.configuration().array )
+            #
+            BAT_converter.Cartesian( BAT_k1_af )
+            self._set_universe_evaluator( lambdas[ state_pair[1] ] )
+            e_k1_af = self.universe.energy() / ( R*lambdas[ state_pair[1] ]['T'] )
+            conf_k1_af = copy.deepcopy( self.universe.configuration().array )
+            #
+            de = ( e_k0_be - e_k0_af ) + ( e_k1_be - e_k1_af )
+            # update confs, energies, BATS
+            if (de > 0) or ( np.random.uniform() < np.exp(de) ):
+              acc_count += 1
+              confs[conf_ind_k0] = conf_k0_af
+              confs[conf_ind_k1] = conf_k1_af
+              #
+              energies[conf_ind_k0] = e_k0_af
+              energies[conf_ind_k1] = e_k1_af
+              #
+              BATs[conf_ind_k0] = BAT_k0_af
+              BATs[conf_ind_k1] = BAT_k1_af
+            #
+            if attempt_count == nr_attempts:
+              return attempt_count, acc_count
+    #
     self._set_lock(process)
 
     if process=='cool':
@@ -2010,6 +2132,22 @@ last modified {2}
       task_queue = m.Queue()
       done_queue = m.Queue()
 
+    # GMC
+    whether_do_gMC = self.params[process]['GMC_attempts'] > 0
+    if whether_do_gMC:
+      print ' '
+      print '  GMC in %s process' %process
+      nr_gMC_attempts = K * self.params[process]['GMC_attempts']
+      torsion_threshold = self.params[process]['GMC_tors_threshold']
+      gMC_attempt_count = 0
+      gMC_acc_count     = 0
+      gMC_time = 0.0
+      BAT_converter, state_indices_to_swap = gMC_initial_setup()
+    else:
+      print ' '
+      print '  Not doing GMC'
+      print ' '
+    #
     # Do replica exchange
     MC_time = 0.0
     repX_time = 0.0
@@ -2045,6 +2183,13 @@ last modified {2}
         # Single process code
         results = [self._sim_one_state(confs[k], process, \
             lambdas[state_inds[k]], doMC[k], False, k) for k in range(K)]
+      # GMC
+      if whether_do_gMC:
+        gMC_start_time = time.time()
+        att_count, acc_count = do_gMC( nr_gMC_attempts, BAT_converter, state_indices_to_swap, torsion_threshold )
+        gMC_attempt_count += att_count
+        gMC_acc_count     += acc_count
+        gMC_time =+ ( time.time() - gMC_start_time )
       # Store results
       for k in range(K):
         if 'acc_MC' in results[k].keys():
@@ -2081,6 +2226,14 @@ last modified {2}
       storage['state_inds'].append(list(state_inds))
       storage['energies'].append(copy.deepcopy(E))
 
+    # GMC
+    if whether_do_gMC:
+      print '  GMC Number of attempts = %d' % gMC_attempt_count
+      print '  GMC Number of accepted attempts = %d' % gMC_acc_count
+      acc_rate = float(gMC_acc_count) / float( gMC_attempt_count ) if gMC_attempt_count > 0 else 0 
+      print '  GMC Acceptance rate = %10.5f' % acc_rate
+      print '  GMC Time spent on gMC = %s' % HMStime(gMC_time)
+      print ' '
     # Estimate relaxation time from empirical state transition matrix
     state_inds = np.array(storage['state_inds'])
     Nij = np.zeros((K,K),dtype=int)
