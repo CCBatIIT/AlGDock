95939cb4a0336337e686692fdf4b4a1053a3ea68
David Minh <daveminh@gmail.com>
2015-08-24 14:40:40 -0500

    Fixed tests
    Removed unnecessary prmtopcrd folder name. Added visualization test.

--- Example/test_python.py
+++ Example/test_python.py
@@ -5,18 +5,18 @@ for run_type in ['cool','dock','postprocess','free_energies']:
   self = AlGDock.BindingPMF_plots.BPMF_plots(\
     dir_dock='dock', dir_cool='cool',\
     ligand_tarball='prmtopcrd/ligand.tar.gz', \
-    ligand_database='prmtopcrd/ligand.db', \
+    ligand_database='ligand.db', \
     forcefield='prmtopcrd/gaff.dat', \
-    ligand_prmtop='prmtopcrd/ligand.prmtop', \
-    ligand_inpcrd='prmtopcrd/ligand.trans.inpcrd', \
+    ligand_prmtop='ligand.prmtop', \
+    ligand_inpcrd='ligand.trans.inpcrd', \
     receptor_tarball='prmtopcrd/receptor.tar.gz', \
-    receptor_prmtop='prmtopcrd/receptor.prmtop', \
-    receptor_inpcrd='prmtopcrd/receptor.trans.inpcrd', \
-    receptor_fixed_atoms='prmtopcrd/receptor.pdb', \
+    receptor_prmtop='receptor.prmtop', \
+    receptor_inpcrd='receptor.trans.inpcrd', \
+    receptor_fixed_atoms='receptor.pdb', \
     complex_tarball='prmtopcrd/complex.tar.gz', \
-    complex_prmtop='prmtopcrd/complex.prmtop', \
-    complex_inpcrd='prmtopcrd/complex.trans.inpcrd', \
-    complex_fixed_atoms='prmtopcrd/complex.pdb', \
+    complex_prmtop='complex.prmtop', \
+    complex_inpcrd='complex.trans.inpcrd', \
+    complex_fixed_atoms='complex.pdb', \
     score = 'prmtopcrd/anchor_and_grow_scored.mol2', \
     dir_grid='grids', \
     protocol='Adaptive', cool_therm_speed=1.5, dock_therm_speed=1.5,\
--- Example/test_terminal.sh
+++ Example/test_terminal.sh
@@ -6,18 +6,18 @@
 
 $ALGDOCK --dir_dock dock --dir_cool cool \
   --ligand_tarball prmtopcrd/ligand.tar.gz \
-  --ligand_database prmtopcrd/ligand.db \
+  --ligand_database ligand.db \
   --forcefield prmtopcrd/gaff.dat \
-  --ligand_prmtop prmtopcrd/ligand.prmtop \
-  --ligand_inpcrd prmtopcrd/ligand.trans.inpcrd \
+  --ligand_prmtop ligand.prmtop \
+  --ligand_inpcrd ligand.trans.inpcrd \
   --receptor_tarball prmtopcrd/receptor.tar.gz \
-  --receptor_prmtop prmtopcrd/receptor.prmtop \
-  --receptor_inpcrd prmtopcrd/receptor.trans.inpcrd \
-  --receptor_fixed_atoms prmtopcrd/receptor.pdb \
+  --receptor_prmtop receptor.prmtop \
+  --receptor_inpcrd receptor.trans.inpcrd \
+  --receptor_fixed_atoms receptor.pdb \
   --complex_tarball prmtopcrd/complex.tar.gz \
-  --complex_prmtop prmtopcrd/complex.prmtop \
-  --complex_inpcrd prmtopcrd/complex.trans.inpcrd \
-  --complex_fixed_atoms prmtopcrd/complex.pdb \
+  --complex_prmtop complex.prmtop \
+  --complex_inpcrd complex.trans.inpcrd \
+  --complex_fixed_atoms complex.pdb \
   --rmsd \
   --dir_grid grids \
   --protocol Adaptive --cool_therm_speed 0.5 --dock_therm_speed 0.5 \
