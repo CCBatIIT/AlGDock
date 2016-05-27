05753dcac2c70c18a6ece356b0c4e7b5609e6634
David Minh <daveminh@gmail.com>
2016-05-27 10:36:53 -0500

    setup for poseFF from main AlGDock directory

--- setup.py
+++ setup.py
@@ -112,6 +112,7 @@ headers = []
 paths = [os.path.join('AlGDock', 'ForceFields', 'Cylinder'),
          os.path.join('AlGDock', 'ForceFields', 'Sphere'),
          os.path.join('AlGDock', 'ForceFields', 'Grid'),
+         os.path.join('AlGDock', 'ForceFields', 'Pose'),
          os.path.join('AlGDock', 'Integrators', 'VelocityVerlet'),
          os.path.join('AlGDock', 'Integrators', 'NUTS'),
          os.path.join('AlGDock', 'Integrators', 'SmartDarting')]
@@ -314,32 +315,36 @@ high_opt.append('-g')
 #################################################################
 
 ext_module_name_and_path = [\
-  ('MMTK_sphere', 'AlGDock/ForceFields/Sphere/MMTK_sphere.pyx'), \
-  ('MMTK_trilinear_grid', 'AlGDock/ForceFields/Grid/MMTK_trilinear_grid.pyx'), \
-  ('MMTK_trilinear_one_fourth_grid', 'AlGDock/ForceFields/Grid/MMTK_trilinear_one_fourth_grid.pyx'), \
-  ('MMTK_BSpline_grid', 'AlGDock/ForceFields/Grid/MMTK_BSpline_grid.pyx'), \
-  ('MMTK_BSpline_transform_grid', 'AlGDock/ForceFields/Grid/MMTK_BSpline_transform_grid.pyx'), \
-  ('NUTS', 'AlGDock/Integrators/NUTS/NUTS.pyx'), \
-  ('NUTS_no_stopping', 'AlGDock/Integrators/NUTS/NUTS_no_stopping.pyx'), \
-  ('SmartDarting', 'AlGDock/Integrators/SmartDarting/SmartDarting.pyx'), \
-  ('BAT', 'Src/BAT.pyx'),
-  ('repX', 'Src/repX.pyx')]
+  ('MMTK_sphere', ['AlGDock/ForceFields/Sphere/MMTK_sphere.pyx']), \
+  ('MMTK_trilinear_grid', ['AlGDock/ForceFields/Grid/MMTK_trilinear_grid.pyx']), \
+  ('MMTK_trilinear_one_fourth_grid', ['AlGDock/ForceFields/Grid/MMTK_trilinear_one_fourth_grid.pyx']), \
+  ('MMTK_BSpline_grid', ['AlGDock/ForceFields/Grid/MMTK_BSpline_grid.pyx']), \
+  ('MMTK_BSpline_transform_grid', ['AlGDock/ForceFields/Grid/MMTK_BSpline_transform_grid.pyx']), \
+  ('MMTK_pose', ['AlGDock/ForceFields/Pose/MMTK_pose.c', \
+    'AlGDock/ForceFields/Pose/pose.c']), \
+  ('NUTS', ['AlGDock/Integrators/NUTS/NUTS.pyx']), \
+  ('NUTS_no_stopping', ['AlGDock/Integrators/NUTS/NUTS_no_stopping.pyx']), \
+  ('SmartDarting', ['AlGDock/Integrators/SmartDarting/SmartDarting.pyx']), \
+  ('BAT', ['Src/BAT.pyx']),
+  ('repX', ['Src/repX.pyx'])]
+
+# PoseFF compilation does not work yet
 
 if False:
   # These extension modules are not used in the current code,
   # but may be used in the future.
   ext_module_name_and_path.extend(\
-    [('MMTK_cylinder','AlGDock/ForceFields/Cylinder/MMTK_cylinder.pyx'), \
-     ('MMTK_trilinear_thresh_grid', 'AlGDock/ForceFields/Grid/MMTK_trilinear_thresh_grid.pyx'), \
-     ('MMTK_trilinear_transform_grid', 'AlGDock/ForceFields/Grid/MMTK_trilinear_transform_grid.pyx')])
+    [('MMTK_cylinder', ['AlGDock/ForceFields/Cylinder/MMTK_cylinder.pyx']), \
+     ('MMTK_trilinear_thresh_grid', ['AlGDock/ForceFields/Grid/MMTK_trilinear_thresh_grid.pyx']), \
+     ('MMTK_trilinear_transform_grid', ['AlGDock/ForceFields/Grid/MMTK_trilinear_transform_grid.pyx'])])
 
 # if False:
 #  These extension modules need to be debugged.
 #  ext_module_name_and_path.extend(\
-#    [('MMTK_CatmullRom_grid', 'AlGDock/ForceFields/Grid/MMTK_CatmullRom_grid.pyx'), \
-#     ('MMTK_CatmullRom_transform_grid', 'AlGDock/ForceFields/Grid/MMTK_CatmullRom_transform_grid.pyx'), \
-#     ('MMTK_tricubic_grid', 'AlGDock/ForceFields/Grid/MMTK_tricubic_grid.pyx'), \
-#     ('MMTK_tricubic_transform_grid', 'AlGDock/ForceFields/Grid/MMTK_tricubic_transformgrid.pyx')])
+#    [('MMTK_CatmullRom_grid', ['AlGDock/ForceFields/Grid/MMTK_CatmullRom_grid.pyx']), \
+#     ('MMTK_CatmullRom_transform_grid', ['AlGDock/ForceFields/Grid/MMTK_CatmullRom_transform_grid.pyx']), \
+#     ('MMTK_tricubic_grid', ['AlGDock/ForceFields/Grid/MMTK_tricubic_grid.pyx']), \
+#     ('MMTK_tricubic_transform_grid', ['AlGDock/ForceFields/Grid/MMTK_tricubic_transformgrid.pyx'])])
 
 setup (name = package_name,
        version = pkginfo.__version__,
@@ -360,6 +365,7 @@ rigid receptor.
                    'AlGDock.ForceFields.Cylinder',
                    'AlGDock.ForceFields.Sphere',
                    'AlGDock.ForceFields.Grid',
+                   'AlGDock.ForceFields.Pose',
                    'AlGDock.Integrators',
                    'AlGDock.Integrators.VelocityVerlet',
                    'AlGDock.Integrators.HamiltonianMonteCarlo',
@@ -368,8 +374,10 @@ rigid receptor.
                    'AlGDock.Integrators.SmartDarting'
                    ],
        ext_package = 'AlGDock.'+sys.platform,
-       ext_modules = [Extension(name, [path], extra_compile_args = compile_args, \
-        include_dirs = include_dirs) for (name,path) in ext_module_name_and_path]
+       ext_modules = [Extension(name, path, \
+        extra_compile_args = compile_args + high_opt, \
+        include_dirs = include_dirs) \
+          for (name,path) in ext_module_name_and_path]
 ,
        data_files = data_files,
        scripts = [],
