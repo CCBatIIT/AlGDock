922f68edab2eb055fa64c91f2ff9994a41cdebfa
David Minh <daveminh@gmail.com>
2016-12-15 13:52:37 -0600

    Eliminated MBAR_c2
    Estimating the solvation free energy with a second order cumulant
    expansion does not converge faster and is less accurate than the
    exponential average.

--- AlGDock/BindingPMF.py
+++ AlGDock/BindingPMF.py
@@ -1584,7 +1584,7 @@ last modified {1}
       # Binding PMF estimates
       self.B = {'MBAR':[]}
       for phase in self.params['dock']['phases']:
-        for method in ['min_Psi','mean_Psi','inverse_FEP','MBAR','MBAR_c2']:
+        for method in ['min_Psi','mean_Psi','inverse_FEP','MBAR']:
           self.B[phase+'_'+method] = []
     if readOnly:
       return True
@@ -1592,7 +1592,7 @@ last modified {1}
     if redo:
       self.B = {'MBAR':[]}
       for phase in self.params['dock']['phases']:
-        for method in ['min_Psi','mean_Psi','inverse_FEP','MBAR','MBAR_c2']:
+        for method in ['min_Psi','mean_Psi','inverse_FEP','MBAR']:
           self.B[phase+'_'+method] = []
       for key in phase_f_RL_keys:
           if key in self.f_RL.keys():
@@ -1715,10 +1715,10 @@ last modified {1}
 
     # BPMFs
     for phase in self.params['dock']['phases']:
-      for key in [phase+'_solv',phase+'_solv_c2']:
+      for key in [phase+'_solv']:
         if not key in self.f_RL:
           self.f_RL[key] = []
-      for method in ['min_Psi','mean_Psi','inverse_FEP','MBAR','MBAR_c2']:
+      for method in ['min_Psi','mean_Psi','inverse_FEP','MBAR']:
         if not phase+'_'+method in self.B:
           self.B[phase+'_'+method] = []
       
@@ -1741,8 +1741,6 @@ last modified {1}
         min_du = min(du)
         #   Exponential average
         f_RL_solv = -np.log(np.exp(-du+min_du).mean()) + min_du
-        #   2nd order cumulant expansion
-        f_RL_solv_c2 = du.mean() - du.var()/2
         
         weights = np.exp(-du+min_du)
         weights = weights/sum(weights)
@@ -1755,7 +1753,6 @@ last modified {1}
         
         # Complex solvation
         self.f_RL[phase+'_solv'].append(f_RL_solv)
-        self.f_RL[phase+'_solv_c2'].append(f_RL_solv_c2)
         
         # Various BPMF estimates
         self.B[phase+'_min_Psi'].append(min_Psi)
@@ -1766,9 +1763,6 @@ last modified {1}
         self.B[phase+'_MBAR'].append(-f_R_solv \
           - self.f_L[phase+'_solv'][-1] - self.f_L['cool_MBAR'][-1][-1] \
           + self.f_RL['grid_MBAR'][-1][-1] + f_RL_solv)
-        self.B[phase+'_MBAR_c2'].append(-f_R_solv \
-          - self.f_L[phase+'_solv'][-1] - self.f_L['cool_MBAR'][-1][-1] \
-          + self.f_RL['grid_MBAR'][-1][-1] + f_RL_solv_c2)
 
         self.tee("  calculated %s binding PMF of %f RT with cycles %d to %d"%(\
           phase, self.B[phase+'_MBAR'][-1], \
