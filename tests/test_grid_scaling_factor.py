115200ed42e03ff004ebc29438108bcb0f81342e
bxie4 <bingxie921@gmail.com>
2025-01-02 22:00:19 -0800

    Updated the test script to include validation for scaling_factors_LJr

--- tests/test_grid_scaling_factor.py
+++ tests/test_grid_scaling_factor.py
@@ -14,6 +14,52 @@ except ImportError:
     print('OpenMM not found.')
     openmm = None
 
+def _load_record(record):
+    """
+    Note: This function originates from the 'prmtop' class in the AlGDock.IO module.
+    """
+    items = []
+    lines = record.split('\n')
+    lines.pop(0)  # Name
+    FORMAT = lines.pop(0).strip()[8:-1]  # Format
+    if FORMAT.find('a') > -1:  # Text
+        w = int(FORMAT[FORMAT.find('a') + 1:])
+        for line in lines:
+            items = items + [line[x:x + w] for x in range(0, len(line), w)]
+        return np.array(items)
+    elif FORMAT.find('I') > -1:  # Integer
+        w = int(FORMAT[FORMAT.find('I') + 1:])
+        for line in lines:
+            items = items + [int(line[x:x + w]) for x in range(0, len(line), w)]
+        return np.array(items, dtype=int)
+    elif FORMAT.find('E') > -1:  # Scientific
+        w = int(FORMAT[FORMAT.find('E') + 1:FORMAT.find('.')])
+        for line in lines:
+            items = items + [float(line[x:x + w]) for x in range(0, len(line), w)]
+        return np.array(items, dtype=float)
+        
+def readPrmtop(FN, varnames=['RESIDUE_LABEL', 'RESIDUE_POINTER']):
+    """
+    Reads an AMBER prmtop file, returning a dictionary
+    Note: This function originates from the 'prmtop' class in the AlGDock.IO module.
+    """
+    if not os.path.isfile(FN):
+        raise Exception('prmtop file %s does not exist!' % FN)
+    if FN.endswith('.gz'):
+        import gzip
+        F = gzip.open(FN, 'r')
+    else:
+        F = open(FN, 'r')
+    data = F.read().split('%FLAG ')
+    F.close()
+    prmtop = {}
+    for record in data:
+        name = record[:record.find('\n')].strip()
+        if name in varnames:
+            prmtop[name] = _load_record(record)
+    return prmtop
+
+
 def generate_benchmark(ligand_database, gaff_forcefield_file, ligand_frcmod_file):
     MMTK.Database.molecule_types.directory = os.path.dirname(os.path.abspath(ligand_database))
     molecule = MMTK.Molecule(os.path.basename(ligand_database))
@@ -72,6 +118,38 @@ if not openmm:
     print('OpenMM not found. Failed to compare. Terminating process.')
     sys.exit(1)
 
+varnames = ['POINTERS','TITLE','ATOM_NAME','AMBER_ATOM_TYPE','CHARGE','MASS',\
+            'NONBONDED_PARM_INDEX','LENNARD_JONES_ACOEF','LENNARD_JONES_BCOEF',\
+            'ATOM_TYPE_INDEX','BONDS_INC_HYDROGEN','BONDS_WITHOUT_HYDROGEN',\
+            'RADII','SCREEN']
+try:
+    import AlGDock
+    import AlGDock.IO
+    prmtop_IO = AlGDock.IO.prmtop()
+    prmtop_alg = prmtop_IO.read(ligand_prmtop, varnames)
+except:
+    prmtop_alg = readPrmtop(ligand_prmtop, varnames)
+    
+NATOM = prmtop_alg['POINTERS'][0]
+NTYPES = prmtop_alg['POINTERS'][1]
+LJ_radius = np.ndarray(shape=(NTYPES), dtype=float)
+LJ_depth = np.ndarray(shape=(NTYPES), dtype=float)
+for i in range(NTYPES):
+    LJ_index = prmtop_alg['NONBONDED_PARM_INDEX'][NTYPES*i+i]-1
+    if prmtop_alg['LENNARD_JONES_ACOEF'][LJ_index]<1.0e-6:
+        LJ_radius[i] = 0
+        LJ_depth[i] = 0
+    else:
+        factor = 2 * prmtop_alg['LENNARD_JONES_ACOEF'][LJ_index] / prmtop_alg['LENNARD_JONES_BCOEF'][LJ_index]
+        LJ_radius[i] = pow(factor, 1.0/6.0) * 0.5
+        LJ_depth[i] = prmtop_alg['LENNARD_JONES_BCOEF'][LJ_index] / 2 / factor
+root_LJ_depth = np.sqrt(LJ_depth)
+LJ_diameter = LJ_radius*2
+atom_type_indicies = [prmtop_alg['ATOM_TYPE_INDEX'][atom_index]-1 for atom_index in range(NATOM)]
+scaling_factor_LJr_dict = dict()
+for (name,type_index) in zip(prmtop_alg['ATOM_NAME'],atom_type_indicies):
+    scaling_factor_LJr_dict[name.strip()] = round(4.184 * root_LJ_depth[type_index]*(LJ_diameter[type_index]**6),6)
+
 prmtop = AmberPrmtopFile(ligand_prmtop)
 inpcrd = AmberInpcrdFile(ligand_inpcrd)
 topology = prmtop.topology
@@ -102,54 +180,21 @@ assert force.getName() == 'NonbondedForce'
 scaling_factors_LJr = []
 scaling_factors_ELE = []
 MMTK_ele_data = []
+MMTK_ljr_data = []
 for i in range(system.getNumParticles()):
-    atom_name = atoms_name[i]
+    atom_name = atoms_name[i].strip()
     charge, sigma, epsilon = force.getParticleParameters(i)
     charge_val = charge.value_in_unit(elementary_charge)
     sigma_val = round(sigma.value_in_unit(nanometer),6)
     epsilon = epsilon.value_in_unit(kilojoule/mole)
     if epsilon == 0:
         continue
-    r = r_distance[atom_name][1]
-    lj_potential = 4 * epsilon * ((sigma_val / r) ** 12 - (sigma_val / r) ** 6)
-    #print(atoms_name[i], '4 *',epsilon, '* ((',sigma_val,'/r ))**12 -', sigma_val, '/r ))**6 =',ljr_data[atom_name])
-    #print(atoms_name[i], round(charge_val*4.184, 6), ele_data[atom_name])
     MMTK_ele_data.append(ele_data[atom_name])
-
-    scaling_factors_LJr.append(lj_potential)
+    MMTK_ljr_data.append(ljr_data[atom_name])
     scaling_factors_ELE.append(round(charge_val*4.184, 6))
+    scaling_factors_LJr.append(scaling_factor_LJr_dict[atom_name])
+
+
 
 assert scaling_factors_ELE == MMTK_ele_data
-
-#TODO: Need to figure out scaling_factors_LJr
-#left of the equal sign: openmm items
-#right of the equal sign: mmtk value
-#r is the estimation
-#H1 4 * 0.06276000026869928 * (( 0.259964 /r ))**12 - 0.259964 /r ))**6 = 316.3363
-#r = 0.1421
-#H2 4 * 0.06276000026869928 * (( 0.259964 /r ))**12 - 0.259964 /r ))**6 = 316.3363
-#r = 0.1421
-#H3 4 * 0.06276000026869928 * (( 0.259964 /r ))**12 - 0.259964 /r ))**6 = 316.3363
-#r = 0.1421
-#H4 4 * 0.06276000026869928 * (( 0.259964 /r ))**12 - 0.259964 /r ))**6 = 316.3363
-#r = 0.1421
-#C1 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#C2 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#C3 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#C4 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#C5 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#C6 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#C7 4 * 0.35982400053705343 * (( 0.339967 /r ))**12 - 0.339967 /r ))**6 = 3788.707674
-#r = 0.1762
-#O1 4 * 0.8786399993381852 * (( 0.295992 /r ))**12 - 0.295992 /r ))**6 = 2578.771323
-#r = 0.1706
-#O2 4 * 0.8786399993381852 * (( 0.295992 /r ))**12 - 0.295992 /r ))**6 = 2578.771323
-#r = 0.1706
-#O3 4 * 0.8803136010405576 * (( 0.306647 /r ))**12 - 0.306647 /r ))**6 = 3191.388968
-#r = 0.1737
+assert scaling_factors_LJr == MMTK_ljr_data
